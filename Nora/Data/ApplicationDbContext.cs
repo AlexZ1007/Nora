4df587ba763ecae7120367a9679f288df992662d
AlexZ1007 <zamfiralexandru1007@gmail.com>
2024-12-05 16:46:04 +0200

    Integrated Roles, SeedData and AplicationUser class

--- Nora/Data/ApplicationDbContext.cs
+++ Nora/Data/ApplicationDbContext.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Nora.Models;
 
 namespace Nora.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>
+        options)
+        : base(options)
         {
         }
+
     }
 }
---
+++ Nora/Models/SeedData.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Nora.Data;
+using System.Collections.Generic;
+
+namespace Nora.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                // Check if roles already exist in the database
+                if (context.Roles.Any())
+                {
+                    return; // Database already contains roles, exit
+                }
+
+                // Create roles in the database
+                context.Roles.AddRange(
+                    new IdentityRole
+                    {
+                        Id = "2c5e174e-3b0e-446f-86af-483d56fd7210",
+                        Name = "Admin",
+                        NormalizedName = "ADMIN"
+                    },
+                    new IdentityRole
+                    {
+                        Id = "2c5e174e-3b0e-446f-86af-483d56fd7211",
+                        Name = "Editor",
+                        NormalizedName = "EDITOR"
+                    },
+                    new IdentityRole
+                    {
+                        Id = "2c5e174e-3b0e-446f-86af-483d56fd7212",
+                        Name = "User",
+                        NormalizedName = "USER"
+                    }
+                );
+
+                // Create a PasswordHasher for hashing passwords
+                var hasher = new PasswordHasher<ApplicationUser>();
+
+                // Create users in the database
+                context.Users.AddRange(
+                    new ApplicationUser
+                    {
+                        Id = "8e445865-a24d-4543-a6c6-9443d048cdb0",
+                        UserName = "admin@test.com",
+                        EmailConfirmed = true,
+                        NormalizedEmail = "ADMIN@TEST.COM",
+                        Email = "admin@test.com",
+                        NormalizedUserName = "ADMIN@TEST.COM",
+                        PasswordHash = hasher.HashPassword(null, "Admin1!")
+                    },
+                    new ApplicationUser
+                    {
+                        Id = "8e445865-a24d-4543-a6c6-9443d048cdb1",
+                        UserName = "editor@test.com",
+                        EmailConfirmed = true,
+                        NormalizedEmail = "EDITOR@TEST.COM",
+                        Email = "editor@test.com",
+                        NormalizedUserName = "EDITOR@TEST.COM",
+                        PasswordHash = hasher.HashPassword(null, "Editor1!")
+                    },
+                    new ApplicationUser
+                    {
+                        Id = "8e445865-a24d-4543-a6c6-9443d048cdb2",
+                        UserName = "user@test.com",
+                        EmailConfirmed = true,
+                        NormalizedEmail = "USER@TEST.COM",
+                        Email = "user@test.com",
+                        NormalizedUserName = "USER@TEST.COM",
+                        PasswordHash = hasher.HashPassword(null, "User1!")
+                    }
+                );
+
+                // Associate users with roles
+                context.UserRoles.AddRange(
+                    new IdentityUserRole<string>
+                    {
+                        RoleId = "2c5e174e-3b0e-446f-86af-483d56fd7210",
+                        UserId = "8e445865-a24d-4543-a6c6-9443d048cdb0"
+                    },
+                    new IdentityUserRole<string>
+                    {
+                        RoleId = "2c5e174e-3b0e-446f-86af-483d56fd7211",
+                        UserId = "8e445865-a24d-4543-a6c6-9443d048cdb1"
+                    },
+                    new IdentityUserRole<string>
+                    {
+                        RoleId = "2c5e174e-3b0e-446f-86af-483d56fd7212",
+                        UserId = "8e445865-a24d-4543-a6c6-9443d048cdb2"
+                    }
+                );
+
+                // Save changes to the database
+                context.SaveChanges();
+            }
+        }
+    }
+}
